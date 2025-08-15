5b5aadaed8b31b22d88d865d1042fa8f1fa3e30a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-15 09:28:27 -0300

    upload project changes

--- src/main/java/br/com/fiap/ovnis/model/Ovnis.java
+++ src/main/java/br/com/fiap/ovnis/model/Ovnis.java
@@ -1,7 +1,10 @@
 package br.com.fiap.ovnis.model;
 
+<<<<<<< HEAD
 import java.time.LocalDateTime;
 
+=======
+>>>>>>> 8fb2c3a74e0b3265f6e1722ed8401f1a8def694c
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +21,7 @@ public class Ovnis {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+<<<<<<< HEAD
     private String location;
     private LocalDateTime date;
     private String color;
@@ -25,12 +29,24 @@ public class Ovnis {
     @Enumerated(EnumType.STRING)
     private Shape shape;
 
+=======
+    private String localizacao;
+    private String data;
+    private String descricao;
+    
+    @Enumerated(EnumType.STRING)
+    private Shape shape;
+    
+>>>>>>> 8fb2c3a74e0b3265f6e1722ed8401f1a8def694c
     public enum Shape {
         DISC,
         SPHERE,
         TRIANGLE,
         OTHER
     }
+<<<<<<< HEAD
 
     private int quantity;
+=======
+>>>>>>> 8fb2c3a74e0b3265f6e1722ed8401f1a8def694c
 }
